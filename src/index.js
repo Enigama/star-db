ff0fda6c4a74859ff568f3c2aa673c98b91d40fb
Eugene <Jeka1.1@mail.ru>
2019-01-11 17:49:56 +0300

    try fetch star-wars api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+const getResource = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Could not fetch ${url}, received ${response.status}`)
+  }
+  const body = await response.json();
+  return body
+}
 
+getResource('https://swapi.co/api/people/1/')
+  .then( body => {
+    console.log(body);
+  })
+.catch(reject => {
+  console.log(reject);
+})
