77fb7d2ff73d335a7963cc0587f36118bef2678b
Eugene Krishtal <Jeka1.1@mail.ru>
2019-01-12 12:52:31 +0300

    stardb connect api, console results

--- src/index.js
+++ src/index.js
@@ -1,16 +1,57 @@
-const getResource = async (url) => {
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`Could not fetch ${url}, received ${response.status}`)
+class SwapiService {
+  _apiBase = 'https://swapi.co/api';
+
+  async getResource(url) {
+      const response = await fetch(`${this._apiBase}${url}`);
+      if (!response.ok) {
+        throw new Error(`Could not fetch ${url}, received ${response.status}`)
+      }
+      return await response.json();
   }
-  const body = await response.json();
-  return body
+
+  getAllPeople() {
+    const resolve =  this.getResource(`/people/`);
+    return resolve.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`)
+  }
+
+  getAllStarships() {
+    const resolve = this.getResource(`/starships/`);
+    return resolve.results;
+  }
+
+  getStarships(id) {
+    return this.getResource(`/starships/${id}/`)
+  }
+
+  getAllPlanets() {
+    const resolve = this.getResource(`/planets/`)
+    return resolve.results;
+  }
+
+  getPlanets(id) {
+    return this.getResource(`/planets/${id}/`)
+  }
 }
 
-getResource('https://swapi.co/api/people/1/')
-  .then( body => {
-    console.log(body);
-  })
-.catch(reject => {
-  console.log(reject);
+const swapi = new SwapiService();
+swapi.getPlanets(8).then(body => {
+  console.log(body.name);
+
+  //each names
+  // body.forEach((v) => {
+  //   console.log(v.name);
+  // });
 })
+
+
+// getResource('https://swapi.co/api/people/1/')
+//   .then( body => {
+//     console.log(body);
+//   })
+// .catch(reject => {
+//   console.log(reject);
+// })
