7b0ba0df966c619d9d9d97e3a975e7bad612a441
Eugene <Jeka1.1@mail.ru>
2019-01-22 17:25:07 +0300

    remove unused variables

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,19 +1,13 @@
 import React from 'react';
-
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import PeoplePage from '../people-page';
-
-import './app.css';
 import ErrorIndicator from "../error-indicator/error-indicator";
 import ErrorButton from "../error-button/error-button";
-import ItemList from "../item-list";
-import ItemDetails, { Record } from "../item-details/item-details";
+import ErrorBoundry from "../error-boundry";
+
 import SwapiService from "../../services/swapi-services";
-import Row from "../row";
 
 import { SwapiServiceProvider} from "../swapi-service-context";
-
 import {
   PersonList,
   PlanetsList,
@@ -22,7 +16,8 @@ import {
   PlanetsDetails,
   StarshipsDetails,
 } from '../sw-components';
-import ErrorBoundry from "../error-boundry";
+
+import './app.css';
 
 class App extends React.Component{
   swapiService = new SwapiService();
